41669f1d1336491c134776e75e70053569341031
Rocka84 <f.dillmeier@gmail.com>
2016-09-12 21:39:27 +0200

    Fixed adding Caches from the map in firefox
    fixes #1

--- dist/CacheTour.user.js
+++ dist/CacheTour.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          CacheTour
 // @namespace     de.rocka84.cachetour
-// @version       0.1.3
+// @version       0.1.4
 // @author        Rocka84 <f.dillmeier@gmail.com>
 // @description   Collect Geocaches from geocaching.com and download them as single GPX file.
 // @run-at        document-end
@@ -1560,18 +1560,6 @@ var console = unsafeWindow.console; //for greasemonkey
 (function(){
 	"use strict";
 
-	CacheTour.addCacheToCurrentTour = function(gc_code, type, name, size, difficulty, terrain) {
-
-		CacheTour.getCurrentTour().addCache(
-				(new CacheTour.Cache(gc_code))
-				.setName(name)
-				.setType(type)
-				.setSize(size.toLowerCase())
-				.setDifficulty(difficulty)
-				.setTerrain(terrain)
-		);
-	};
-
 	CacheTour.registerModule(
 		new CacheTour.Module({
 			name: 'Map',
@@ -1586,9 +1574,45 @@ var console = unsafeWindow.console; //for greasemonkey
 				template.html(template.html().replace(
 					/<span>Log Visit<\/span>/,
 					"<span>Log Visit</span></a>\n" +
-					"<a class=\"lnk cachetour-add\" href=\"#\" onclick='CacheTour.addCacheToCurrentTour(\"{{=gc}}\", \"{{=type.value}}\", \"{{=name}}\", \"{{=container.text}}\", \"{{=difficulty.text}}\", \"{{=terrain.text}}\");return false;'>\n" +
+					"<a class=\"lnk cachetour_add_to_tour\" href=\"#\" " +
+						"onclick=\"$('#cachetour_add_to_tour').click(); return false;\" " +
+						"data-gc-code=\"{{=gc}}\" " +
+						"data-type=\"{{=type.value}}\" " +
+						"data-name=\"{{=name}}\" " +
+						"data-size=\"{{=container.text}}\" " +
+						"data-difficulty=\"{{=difficulty.text}}\" " +
+						"data-terrain=\"{{=terrain.text}}\"" +
+					">\n" +
 					"<img src=\"/images/icons/16/write_log.png\"><span>Add to Tour</span>"
 				));
+				
+				$(unsafeWindow.document.body).delegate('.cachetour_add_to_tour', 'click', function() {
+					console.log($(this));
+				});
+
+				/*
+				 * In Firefox, the sandbox is quite restrictive, so you can't add event handlers
+				 * to elements you didn't create yourself. But you may trigger events on elements
+				 * you created from outside of the sandbox.
+				 * So here I create one static element, whose click event is triggered by the
+				 * links that are created from the template above.
+				 * Maybe someday I'll find a better solution for this, but this works for now.
+				 */
+				CacheTour.addStyle('#cachetour_add_to_tour { display:none; }');
+				var add_btn = $('<button id="cachetour_add_to_tour">Add Cache</button>').appendTo(document.body).click(function(){
+					var cache_link = $('.cachetour_add_to_tour');
+					console.log(cache_link);
+					if (cache_link && cache_link.attr('data-gc-code')) {
+						CacheTour.getCurrentTour().addCache(
+								(new CacheTour.Cache(cache_link.attr('data-gc-code')))
+								.setName(cache_link.attr('data-name'))
+								.setType(cache_link.attr('data-type'))
+								.setSize(cache_link.attr('data-size').toLowerCase())
+								.setDifficulty(cache_link.attr('data-difficulty'))
+								.setTerrain(cache_link.attr('data-terrain'))
+						);
+					}
+				});
 			}
 		})
 	);
